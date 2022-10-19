840d128abc1abfb7b618e63555ce1f179b4f7584
Radhey Parekh <radhey.parekh@gmail.com>
2022-10-19 19:24:55 +0530

    Light-Dark Theme POC

--- toggle-theme/src/App.js
+++ toggle-theme/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "react-bootstrap/Card";
+import { createContext, useState } from "react";
+import Switch from "react-switch";
+// import Button from "react-bootstrap/Button";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((theme) => (theme === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <Card className="text">
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Switch onChange={toggleTheme} checked={theme === "dark"} />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
